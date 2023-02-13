d1850be4e4ae0ffa1f8dbb9dd4181cfa0a5dea51
bereter <118691981+bereter@users.noreply.github.com>
2023-02-13 23:13:17 +0300

    home work

---
+++ new_portal/NewsPaper/news/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    user_author = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        rating_posts_author = Post.objects.filter(author_post_id=self.pk).aggregate(rating_news=Sum('rating_news'))['rating_news']
+        rating_comments_author = \
+        Comment.objects.filter(user_comment_id=self.pk).aggregate(rating_comment=Sum('rating_comment'))[
+            'rating_comment']
+        rating_comments_posts = \
+        Comment.objects.filter(post_comment__author_post=self.pk).aggregate(rating_comment=Sum('rating_comment'))[
+            'rating_comment']
+        self.user_rating = rating_posts_author * 3 + rating_comments_author + rating_comments_posts
+        self.save()
+
+
+class Category(models.Model):
+    world_events = 'WE'
+    politics = 'PO'
+    culture = 'CU'
+    economics = 'EC'
+    technology = 'TE'
+    science = 'SC'
+
+    CATEGORY_NEWS = [
+        (world_events, 'События в мире'),
+        (politics, 'Политика'),
+        (culture, 'Культура'),
+        (economics, 'Экономика'),
+        (technology, 'Технологии'),
+        (science, 'Наука')
+    ]
+
+    category_new = models.CharField(max_length=2, choices=CATEGORY_NEWS, unique=True)
+
+
+class Post(models.Model):
+    news = 'NE'
+    article = 'AR'
+    FIELD = [
+        (news, 'Новость'),
+        (article, 'Статья')
+    ]
+
+    article_or_news = models.CharField(max_length=2, choices=FIELD)
+    data_time = models.DateTimeField(auto_now_add=True)
+    name_news = models.CharField(max_length=255)
+    text_news = models.TextField()
+    rating_news = models.IntegerField(default=0)
+    author_post = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_category = models.ManyToManyField(Category, through='PostCategory')
+
+    def like_post(self):
+        self.rating_news += 1
+        self.save()
+
+    def dislike_post(self):
+        self.rating_news -= 1
+        self.save()
+
+    def preview(self):
+        return self.text_news[0:124] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    text_comment = models.TextField()
+    data_time_comment = models.DateTimeField(auto_now_add=True)
+    rating_comment = models.IntegerField(default=0)
+    post_comment = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_comment = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def like_comment(self):
+        self.rating_comment += 1
+        self.save()
+
+    def dislike_comment(self):
+        self.rating_comment -= 1
+        self.save()
